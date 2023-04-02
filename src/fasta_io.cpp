d887d41861f8d8ff60af412bc45a2d251203c25d
Trent Hauck <trent@trenthauck.com>
2023-04-02 13:37:53 -0700

    feat: add glob support

--- src/fasta_io.cpp
+++ src/fasta_io.cpp
@@ -14,7 +14,9 @@ namespace fasql
 
     struct FastaScanBindData : public duckdb::TableFunctionData
     {
-        std::string file_path;
+        std::vector<std::string> file_paths;
+        int nth_file = 0;
+
         klibpp::SeqStreamIn *stream;
     };
 
@@ -50,18 +52,18 @@ namespace fasql
                                                        std::vector<duckdb::LogicalType> &return_types, std::vector<std::string> &names)
     {
         auto result = duckdb::make_unique<FastaScanBindData>();
-        auto file_name = input.inputs[0].GetValue<std::string>();
-
         auto &fs = duckdb::FileSystem::GetFileSystem(context);
 
-        if (!fs.FileExists(file_name))
+        auto glob = input.inputs[0].GetValue<std::string>();
+        std::vector<std::string> glob_result = fs.Glob(glob);
+        if (glob_result.size() == 0)
         {
-            throw duckdb::IOException("File does not exist: " + file_name);
+            throw duckdb::IOException("No files found for glob: " + glob);
         }
+        result->file_paths = glob_result;
+        result->stream = new klibpp::SeqStreamIn(result->file_paths[0].c_str());
 
-        result->file_path = file_name;
-        result->stream = new klibpp::SeqStreamIn(result->file_path.c_str());
-
+        return_types.push_back(duckdb::LogicalType::VARCHAR);
         return_types.push_back(duckdb::LogicalType::VARCHAR);
         return_types.push_back(duckdb::LogicalType::VARCHAR);
         return_types.push_back(duckdb::LogicalType::VARCHAR);
@@ -69,13 +71,14 @@ namespace fasql
         names.push_back("id");
         names.push_back("description");
         names.push_back("sequence");
+        names.push_back("file_name");
 
         return std::move(result);
     }
 
     void FastaScan(duckdb::ClientContext &context, duckdb::TableFunctionInput &data, duckdb::DataChunk &output)
     {
-        auto bind_data = (const FastaScanBindData *)data.bind_data;
+        auto bind_data = (FastaScanBindData *)data.bind_data;
         auto local_state = (FastaScanLocalState *)data.local_state;
 
         if (local_state->done)
@@ -84,6 +87,8 @@ namespace fasql
         }
 
         auto stream = bind_data->stream;
+        auto nth_file = bind_data->nth_file;
+        auto current_file = bind_data->file_paths[nth_file];
         auto records = stream->read(STANDARD_VECTOR_SIZE);
 
         auto read_records = 0;
@@ -102,15 +107,25 @@ namespace fasql
             }
 
             output.SetValue(2, output.size(), duckdb::Value(record.seq));
+            output.SetValue(3, output.size(), duckdb::Value(current_file));
 
             output.SetCardinality(output.size() + 1);
 
             read_records++;
         }
 
+        // We have read all records from the current file, check if we have more files or are done.
         if (read_records < STANDARD_VECTOR_SIZE)
         {
-            local_state->done = true;
+            if (bind_data->nth_file < bind_data->file_paths.size() - 1)
+            {
+                bind_data->nth_file++;
+                bind_data->stream = new klibpp::SeqStreamIn(bind_data->file_paths[bind_data->nth_file].c_str());
+            }
+            else
+            {
+                local_state->done = true;
+            }
         }
     };
 
@@ -136,10 +151,11 @@ namespace fasql
 
         auto &fs = duckdb::FileSystem::GetFileSystem(context);
 
-        if (!(fs.FileExists(table_name)))
+        std::vector<std::string> glob_result = fs.Glob(table_name);
+        if (glob_result.size() == 0)
         {
             return nullptr;
-        };
+        }
 
         std::vector<duckdb::unique_ptr<duckdb::ParsedExpression>> children;
         children.push_back(duckdb::make_unique<duckdb::ConstantExpression>(duckdb::Value(table_name)));
